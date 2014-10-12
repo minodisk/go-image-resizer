c0dfc9a178a0dd7d74e6a5533d566c8f2f551f3b
minodisk <daisuke.mino@gmail.com>
2014-10-13 02:05:41 +0900

    Add main

---
+++ resizer/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"image"
+	"image/png"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/nfnt/resize"
+)
+
+type Req struct {
+	Uri   string `json:"uri"`
+	Width uint   `json:"width"`
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	if err := http.ListenAndServe(":9000", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	buf := new(bytes.Buffer)
+	buf.ReadFrom(r.Body)
+
+	dec := json.NewDecoder(buf)
+	var req Req
+	dec.Decode(&req)
+	fmt.Printf("%+v\n", req)
+
+	img := fetchImage(req.Uri)
+	m := resize.Resize(req.Width, 0, img, resize.Lanczos3)
+
+	out, err := os.Create("storage/test.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer out.Close()
+	png.Encode(out, m)
+}
+
+func fetchImage(uri string) image.Image {
+	res, err := http.Get(uri)
+	if err != nil || res.StatusCode != 200 {
+		log.Fatal("Doesn't exist")
+	}
+
+	defer res.Body.Close()
+	img, _, err := image.Decode(res.Body)
+	if err != nil {
+		log.Fatal("Can't decode")
+	}
+
+	return img
+}
