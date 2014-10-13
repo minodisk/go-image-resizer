9755f1ce91e24ebb3715f51170c34969dac87bd1
minodisk <daisuke.mino@gmail.com>
2014-10-13 21:57:04 +0900

    Move main() to the head

--- main.go
+++ main.go
@@ -18,6 +18,16 @@ type Req struct {
 	Width uint   `json:"width"`
 }
 
+func main() {
+	log.Println("main")
+	port := os.Getenv("PORT")
+	http.HandleFunc("/", IndexHandler)
+	log.Printf("Start listening on port %s", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(r.Body)
@@ -53,13 +63,4 @@ func fetchImage(uri string) image.Image {
 	}
 
 	return img
-}
-
-func main() {
-	port := os.Getenv("PORT")
-	http.HandleFunc("/", IndexHandler)
-	log.Printf("Start listening on port %s", port)
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
-		log.Fatal("ListenAndServe: ", err)
-	}
 }
